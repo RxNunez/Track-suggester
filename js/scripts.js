1263ef35281e59a4b294340dd705dad818babece
RxNunez <Nunez.ryan@gmail.com>
2017-07-11 22:18:03 -0700

    Include indention and js script correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,74 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
-    var platform = $("input#platform").val();
-    var industry = $("input#industry").val();
-    var end = $("input#end").val();
+    var platform = $("select#platform").val();
+    var industry = $("select#industry").val();
+    var end = $("select#end").val();
 
 
     if (platform === 'windows') {
       if (industry === 'business' && end === 'front') {
-        $("#image").attr("src","img/CSS.jpg");
-      }
-      else if (industry === 'gaming' && end === 'back') {
-        $("#image").attr("src","img/C#.jpg");
-      }
-      else if (industry === 'mobile' && end === 'design') {
         $("#image").attr("src","img/Java.jpg");
       }
-    }
-
-    else if (platform === 'mac') {
-      if (industry === 'business' && end === 'front') {
-        $("#image").attr("src","img/CSS.jpg");
-      }
       else if (industry === 'gaming' && end === 'back') {
-        $("#image").attr("src","img/C#.jpg");
+        $("#image").attr("src","img/csharp.jpg");
       }
       else if (industry === 'mobile' && end === 'design') {
+        $("#image").attr("src","img/CSS.jpg");
+      }
+      else if (industry === 'gaming' && end === 'front') {
         $("#image").attr("src","img/Java.jpg");
       }
+      else if (industry === 'mobile' && end === 'back') {
+        $("#image").attr("src","img/csharp.jpg");
+      }
+      else if (industry === 'business' && end === 'design') {
+        $("#image").attr("src","img/CSS.jpg");
+      }
+      else if (industry === 'mobile' && end === 'front') {
+        $("#image").attr("src","img/Java.jpg");
+      }
+      else if (industry === 'business' && end === 'back') {
+        $("#image").attr("src","img/csharp.jpg");
+      }
+      else if (industry === 'gaming' && end === 'design') {
+        $("#image").attr("src","img/CSS.jpg");
+      }
+
     }
 
+      else if (platform === 'mac') {
+        if (industry === 'business' && end === 'front') {
+        $("#image").attr("src","img/Java.jpg");
+      }
+      else if (industry === 'gaming' && end === 'back') {
+        $("#image").attr("src","img/csharp.jpg");
+      }
+      else if (industry === 'mobile' && end === 'design') {
+        $("#image").attr("src","img/CSS.jpg");
+      }
+      else if (industry === 'gaming' && end === 'front') {
+        $("#image").attr("src","img/Java.jpg");
+      }
+      else if (industry === 'mobile' && end === 'back') {
+        $("#image").attr("src","img/csharp.jpg");
+      }
+      else if (industry === 'business' && end === 'design') {
+        $("#image").attr("src","img/CSS.jpg");
+      }
+      else if (industry === 'mobile' && end === 'front') {
+        $("#image").attr("src","img/Java.jpg");
+      }
+      else if (industry === 'business' && end === 'back') {
+        $("#image").attr("src","img/csharp.jpg");
+      }
+      else if (industry === 'gaming' && end === 'design') {
+        $("#image").attr("src","img/CSS.jpg");
+      }
+
+    }
+    else {
+         $("#image").attr("src","img/PhP.jpg");
+       }
   event.preventDefault();
   });
 });
